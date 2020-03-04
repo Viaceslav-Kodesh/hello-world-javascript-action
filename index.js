22fa91d32f3622c4fb1333ab6db7103202eff3ee
Viaceslav-Kodesh <54391135+Viaceslav-Kodesh@users.noreply.github.com>
2020-03-04 13:46:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import { ToolRunner, argStringToArray } from "@actions/exec/lib/toolrunner";
+import OpenStack from 'js-openstack-lib';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const exec = require('@actions/exec');
+
+// Initialize cloud
+// cloudConfig is a JSON object corresponding to clouds.yaml
+// (It is your responsibility to load and parse it)
+
+
+
+async function openstackConnect() {
+    const userName = core.getInput('OS_USERNAME');
+    const authUrl = core.getInput('OS_AUTH_URL');
+    const projectDomainName = core.getInput('PROJECT_DOMAIN_NAME');
+    const userPassword = core.getInput('OS_PASSWORD');
+    const userDomainName = core.getInput('OS_USER_DOMAIN_NAME');
+    const projectName = core.getInput('OS_PROJECT_NAME');
+    const clusterName = core.getInput('CLUSTER_NAME');
+
+    const openStack = new OpenStack({
+        region_name: 'Region1',
+        auth: {
+          username: userName,
+          password: userPassword,
+          project_name: projectName,
+          auth_url: authUrl
+        }
+      });
+      // List all flavors
+      openStack.networkList()
+        .then((networks) => {
+          console.log(networks);
+        });
+    }
+
+async function exportKubeconfig() {
+    core.exportVariable('KUBECONFIG', './config');
+  }
+
+// test run: kubectl cluster-info
+async function kubectl() {
+    await exec.exec('kubectl cluster-info');
+    }
+
+async function run() {
+    await openstackConnect();
 }
+
+run();
